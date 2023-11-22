f1a135fa31cc012a0bc993f3f2328daf719696e0
User <elxan.memmedli2003@gmail.com>
2023-11-22 21:04:29 +0400

    Title commit

--- Access Modifiers/Program.cs
+++ Access Modifiers/Program.cs
@@ -6,6 +6,22 @@ namespace Access_Modifiers
     {
         static void Main(string[] args)
         {
+            /*Employee class
+             - Name
+             - Surname
+             - Salary
+
+
+            Department class
+             - Name
+             - EmployeeLimit
+             - SalaryLimit
+             - [] Employees
+             - AddEmployee()
+
+
+             Employee ve Department classlarını ClassLibrary-de yazın. Employee Name və Surname dəyərləri yalnız hərflərdən ibarət olmalıdır və böyük hərflə başlamalıdır. Salary dəyəri 250-dən aşağı ola bilməz. AddEmployee metodu employees array-e employee obyekti əlavə etmək üçündür.*/
+
             Employee employee1 =  new Employee("Elxan","Mammadli",500);
             Employee employee2 =  new Employee("Murad","Babayev",500);
             Employee employee3 =  new Employee("Zerife","Memmedov",500);
